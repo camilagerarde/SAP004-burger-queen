6578cdfd3a4cd793de4d1fe003f06cce088dd8b5
Sofia <sofiasimas1992@gmail.com>
2020-07-21 16:34:02 -0300

    add login

---
+++ src/pages/Login/LoginForm/index.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+//perguntar
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const changeEmail = (element) => {
+    setEmail(element.target.value)
+  }
+
+  const changePassword = (element) => {
+    setPassword(element.target.value)
+  }
+  return (
+    <>
+      <Input 
+        onChange={changeEmail}
+        label="email"
+        id="email"
+        type="text"
+        value={email}
+        placeholder="teste@teste.com"
+      />
+      <Input
+        onChange={changePassword}
+        label="senha"
+        id="senha"
+        type="password"
+        value={password}
+      />
+      <Button>Entrar</Button>
+    </>
+  )
+}
+
+export default LoginForm;
