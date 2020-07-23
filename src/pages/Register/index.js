411792e37f5c4d38a610ad5e9683c736d5d02e82
Camila Cunha <millagerarde@gmail.com>
2020-07-22 22:19:11 -0300

    Alinhamento da imagem do logo e nome

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,16 +1,14 @@
 import React from "react";
-import MrKing from "../../images/logo.png";
-import FormRegister from "../../components/FormRegister";
+import LogoPresentation from "../../components/LogoPresentation";
+import FormRegister from "./FormRegister/";
 import style from "./style.module.css";
 
 function Register() {
   return (
-    <div className={style.sobre}>
-      <figure>
-        <img src={MrKing} className={style.img} alt="logo" />
-      </figure>
+    <section className={style.container}>
+      <LogoPresentation />
       <FormRegister />
-    </div>
+    </section>
   );
 }
 
