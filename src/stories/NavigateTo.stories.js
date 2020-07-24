afbbe5ccca11884565a70d2f3fae1212d0394e3d
Sofia <sofiasimas1992@gmail.com>
2020-07-24 15:15:42 -0300

    add navigate

---
+++ src/stories/NavigateTo.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NavigateTo from "../components/NavigateTo";
+
+
+export default {
+  title: "NavigateTo",
+  component: NavigateTo,
+};
+
+export const Default = () => <NavigateTo/>;
