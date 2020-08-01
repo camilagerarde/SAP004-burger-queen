5622bdb9ebe8da766570e6b1c430f17973351395
Sofia <sofiasimas1992@gmail.com>
2020-08-01 16:07:46 -0300

    stories loginForm

--- src/stories/LoginForm.stories.js
+++ src/stories/LoginForm.stories.js
@@ -1,9 +1,16 @@
 import React from "react";
 import LoginForm from "../pages/Login/LoginForm";
+import { Link } from "react-router-dom";
 
 export default {
   title: "LoginForm",
   component: LoginForm,
 };
 
-export const Default = () => <LoginForm />;
+export const Default = () => (
+<LoginForm>
+  <Link to="/register" title="Registre-se">
+  Não possui conta? Registre-se
+  </Link>
+</LoginForm>
+)
