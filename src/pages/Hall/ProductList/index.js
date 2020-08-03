4aee01401b1b7d4efdf5b7e5160ccba778607b76
Sofia <sofiasimas1992@gmail.com>
2020-08-03 14:47:37 -0300

    add section, onclick, color, size button

--- src/pages/Hall/ProductList/index.js
+++ src/pages/Hall/ProductList/index.js
@@ -1,20 +1,43 @@
 import React from "react";
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 import Card from "../../../components/Card";
-import Product from "../../../components/Product"
-
+import Product from "../../../components/Product";
+import Button from "../../../components/Button";
+import style from  "./style.module.css";
 
 const ProductList = (props) => {
   return(
     <Card>
-      {props.products.map((prod) => (
-        <Product
-          name={prod.name}
-          price={prod.price}
-          key={prod.name}
-          type={prod.type}
-        />
-      ))}
+      <>
+        <section
+        className={style.buttonMenu}>
+          <Button
+            onClick={() => props.onChangeCategory('breakfast')}
+            color="salmon"
+            size="small"
+          >
+            Café da Manhã
+          </Button>
+          <Button
+            onClick={() => props.onChangeCategory('lunch')}
+            color="salmon"
+            size="small"
+          >
+            Almoço e jantar
+          </Button>
+        </section>
+        <section
+        className={style.cardMenu}>
+        {props.products.map((prod) => (
+          <Product
+            name={prod.name}
+            price={prod.price}
+            key={prod.name}
+            type={prod.type}
+          />
+        ))}
+        </section>
+      </>
     </Card>
   )
 }
