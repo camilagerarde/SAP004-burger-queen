6f0efc65f078e097a9dfc26c2f49deda4a70af4d
Sofia <sofiasimas1992@gmail.com>
2020-08-03 14:49:32 -0300

    add router stories

--- src/stories/NavComponent.stories.js
+++ src/stories/NavComponent.stories.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {BrowserRouter} from "react-router-dom";
 import NavComponent from "../components/NavComponent";
 import NavItem from "../components/NavItem";
 
@@ -9,9 +10,11 @@ export default {
 };
 
 export const Default = () =>  (
-  <NavComponent>
-    <NavItem to="/hall/newOrder">Pedidos Novos</NavItem>
-    <NavItem to="/hall/ready">Pedidos Prontos</NavItem>
-    <NavItem to="/hall/delivered">Pedidos entregues</NavItem>
-  </NavComponent>
+  <BrowserRouter>
+    <NavComponent>
+      <NavItem to="/hall/newOrder">Pedidos Novos</NavItem>
+      <NavItem to="/hall/ready">Pedidos Prontos</NavItem>
+      <NavItem to="/hall/delivered">Pedidos entregues</NavItem>
+    </NavComponent>
+  </BrowserRouter>
 );
