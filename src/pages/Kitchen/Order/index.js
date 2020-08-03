0f182c365a3cab52fa485021c38f33dd3e42fb3a
Camila Cunha <millagerarde@gmail.com>
2020-08-03 17:38:22 -0300

    Criação do componente order para os pedidos

---
+++ src/pages/Kitchen/Order/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './style.module.css';
+
+const Card = (props) => {
+  return (
+    <section
+      className={style.card}
+    >
+      {props.children}
+    </section>
+  )
+}
+
+export default Card;
