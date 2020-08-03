98e7dc4bb70227b38c60fd77aaad529f8bb3de16
Camila Cunha <millagerarde@gmail.com>
2020-08-03 17:03:30 -0300

    Cosntrução page kitchen

--- src/pages/Kitchen/index.js
+++ src/pages/Kitchen/index.js
@@ -1,10 +1,28 @@
 import React from "react";
+import NavComponent from "../../components/NavComponent";
+import NavItem from "../../components/NavItem";
 import style from "./style.module.css";
+import Card from "../../components/Card";
 
 function Kitchen() {
   return (
     <section className={style.container}>
-      <h1>COZINHA</h1>
+      <NavComponent>
+        <NavItem to="/kitchen/inProgress">Pedidos aguardando</NavItem>
+        <NavItem to="/kitchen/ready">Pedidos entregues</NavItem>
+      </NavComponent>{" "}
+      <div className={style.main}>
+        <Card>
+          {" "}
+          <h3>Pedido 01</h3>
+          <p> Em construção</p>
+        </Card>
+        <Card>
+          {" "}
+          <h3>Pedido 02</h3>
+          <p> Em construção</p>
+        </Card>
+      </div>
     </section>
   );
 }
