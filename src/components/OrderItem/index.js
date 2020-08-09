3edf1d1059ab2488d153ef195868369b84314d47
camilagerarde <millagerarde@gmail.com>
2020-08-09 15:27:28 -0300

    icones pedido

--- src/components/OrderItem/index.js
+++ src/components/OrderItem/index.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types'
-import Button from '../Button';
+import React from "react";
+import PropTypes from "prop-types";
+import Ico from "../Ico";
+import style from "./style.module.css";
 
 const OrderItem = (props) => {
   return (
     <li>
-      {props.name}
-      <Button
-      onClick={props.onRemove}>-</Button>
-      {props.count}
-      <Button
-      onClick={props.onAdd}>+</Button>
+      <p className={style.itens}>
+        {props.name}
+        <Ico onClick={props.onRemove} type="minus" alt="Diminuir quantidade" />
+        {props.count}
+        <Ico onClick={props.onAdd} type="more" alt="Aumentar quantidade" />
+        {props.price.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
+        <Ico type="del" alt="Excluir item" />
+      </p>
     </li>
-  )
-}
+  );
+};
 
 OrderItem.propTypes = {
   onAdd: PropTypes.func.isRequired,
   onRemove: PropTypes.func.isRequired,
   name: PropTypes.string.isRequired,
   count: PropTypes.number.isRequired,
-}
+};
 
-
-export default OrderItem
+export default OrderItem;
