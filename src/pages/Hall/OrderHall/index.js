560d05f18a2ce566ea33cf0cc8d2dddf3ec10f50
camilagerarde <millagerarde@gmail.com>
2020-08-09 15:59:27 -0300

    background input

--- src/pages/Hall/OrderHall/index.js
+++ src/pages/Hall/OrderHall/index.js
@@ -1,39 +1,36 @@
-import React from 'react';
-import style from './style.module.css';
-import Input from '../../../components/Input';
-import Button from '../../../components/Button';
-import OrderItem from '../../../components/OrderItem';
+import React from "react";
+import style from "./style.module.css";
+import Input from "../../../components/Input";
+import Button from "../../../components/Button";
+import OrderItem from "../../../components/OrderItem";
 
 const OrderHall = (props) => {
   const onChangeName = (event) => {
     const name = event.target.value;
     props.dispatch({
-      type:'changeName',
-      payload:{name}
-    })
-  }
+      type: "changeName",
+      payload: { name },
+    });
+  };
 
   const onChangeTable = (event) => {
     const table = event.target.value;
     props.dispatch({
-      type:'changeTable',
-      payload:{table}
-    })
-  }
+      type: "changeTable",
+      payload: { table },
+    });
+  };
 
   return (
-    <section
-      className={style.card}
-    >
-      <h2>
-        Resumo do pedido
-      </h2>
+    <section className={style.card}>
+      <h2>Resumo do pedido</h2>
       <Input
         onChange={onChangeName}
         label="atendente"
         id="nameOrder"
         type="text"
         value={props.order.name}
+        border="border"
       />
       <Input
         onChange={onChangeTable}
@@ -41,22 +38,25 @@ const OrderHall = (props) => {
         id="numberTable"
         type="number"
         value={props.order.table}
+        border="border"
       />
       <ul>
-      {props.order.products.map((prod) => (
-        <OrderItem
-        name={prod.name}
-        count={prod.count}
-        key={prod.name}
-        onAdd={() => props.onAddProduct(prod)}
-        onRemove={() => props.onRemoveProduct(prod)}
-        />
-      ))}
+        {props.order.products.map((prod) => (
+          <OrderItem
+            name={prod.name}
+            price={prod.price}
+            count={prod.count}
+            key={prod.name}
+            onAdd={() => props.onAddProduct(prod)}
+            onRemove={() => props.onRemoveProduct(prod)}
+          />
+        ))}
       </ul>
-      <h3>
-      {`Total ${props.order.total}`}
-      </h3>
-      <Button 
+      <h3>{`Total ${props.order.total.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      })}`}</h3>
+      <Button
         type="send"
         className={style.buttonSend}
         onClick={() => props.onChangeStatus()}
@@ -65,8 +65,26 @@ const OrderHall = (props) => {
       </Button>
       {props.children}
     </section>
-    
-  )
-}
+  );
+};
 
 export default OrderHall;
+
+// const removeProduct = (product) => {
+//   const delProduct = order.filter((elem) => elem !== product);
+//   setOrder([...delProduct]);
+//   calculateTotal(product, "remove");
+// };
+
+// .toLocaleString("pt-BR", {
+//   style: "currency",
+//   currency: "BRL",
+// })
+
+// <Ico
+//                     onClick={() => decreaseProduct(prod)}
+//                     type="minus"
+//                     alt="Diminuir quantidade"
+//                   />
+
+// new Date().getTime(),
