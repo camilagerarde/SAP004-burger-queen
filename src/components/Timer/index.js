007a1df4d46f7e2c4b67868372bef0fb38b58bcb
Sofia <sofiasimas1992@gmail.com>
2020-08-14 13:55:18 -0300

    timer

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,23 +1,33 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
+import dayjs from "dayjs";
+import style from "./style.module.css";
 
+const dateDiff = (startDate, endDate) => {
+  const endDay = dayjs(endDate)
+
+  const hours = endDay.diff(startDate, 'hour')
+  const diffMinutes = endDay.diff(startDate, 'minute')
+  const diffSeconds = endDay.diff(startDate, 'second') 
+  let minutes = diffMinutes;
+  if (hours >= 1 ){
+    minutes = diffMinutes % (60*hours)
+  }
+  let seconds = diffSeconds;
+  if (minutes >= 1){
+    seconds = diffSeconds % (60*minutes)
+  }
+
+  return`${hours}:${minutes}:${seconds}`
+}
 
 const Timer = (props) => {
-  const [elapsedTime, setElapsedTime] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setElapsedTime(elapsedTime+1)
-    }, 1000);
-    return () => {
-      clearInterval(interval);
-    }
-  }, [elapsedTime, setElapsedTime]); //firebase
-
-
   return (
     <>
-      <div>{elapsedTime}</div>
-      <div>{props.createdAt}</div>
+      {
+        props.status === "ready"
+        ? (<span className={style.time}>Tempo do pedido: {dateDiff(props.createdAt, props.deliveredAt)}</span>)
+        : (<span className={style.time}>Tempo do pedido: {dateDiff(props.createdAt, new Date().getTime())}</span>)
+      }
     </>
   )
 }
